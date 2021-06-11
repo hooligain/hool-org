dbfe4f27504b855bd5298b0700b13c714af05ab5
giabao5995 <giabao5995@gmail.com>
2021-06-11 08:15:08 +0200

    feat (milestones) creation milestones section

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -13,6 +13,8 @@ import Ecosystem from "./ecosystem.png";
 import EcosystemMobile from "./ecosystem-mobile.png";
 import EcosystemBg from "./ecosystem-bg.png";
 import EcosystemBgMb from "./ecosystem-bg-mb.png";
+import Milestones from "./milestones.png";
+import MilestonesMb from "./milestones-mb.png";
 
 export {
     Separation,
@@ -29,5 +31,7 @@ export {
     Ecosystem,
     EcosystemMobile,
     EcosystemBg,
-    EcosystemBgMb
+    EcosystemBgMb,
+    Milestones,
+    MilestonesMb
 };
