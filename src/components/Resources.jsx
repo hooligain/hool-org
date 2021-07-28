548b02ad520b4a4477865533baf77eee239f229a
Phu Quoc DINH <phuquoc.dinh@bienici.com>
2021-07-28 21:33:14 +0200

    feat: update page resources

---
+++ src/components/Resources.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './Header';
+import {ListResources} from '../helpers/constants';
+
+const Resources = () => {
+	return (
+		<>
+			<Header />
+			<div className='resources'>
+				{ListResources.map((resource, index) => (
+					<a key={index} className='resources__item' href={resource.link} rel='noreferrer' target='_blank'>
+						<img className='icon' src={resource.icon} alt='pdf file' />
+						<span className='resources__item__name'>{resource.name}</span>
+					</a>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default Resources;
