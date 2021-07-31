ce112c562000bb12c10ebcc65917565454607a93
Bao <bao@Hoangs-MacBook-Pro.local>
2021-07-31 10:48:03 +0200

    feat (hool resource) add pitch deck file

--- src/assets/pdf/index.js
+++ src/assets/pdf/index.js
@@ -2,5 +2,6 @@ import hool_gameplay from './Hool_gameplay.pdf';
 import hool_tokenomic from './Hool_Tokenomic.pdf';
 import hool_loyalty_farming_protocol from './Hool_Loyalty_farming_protocol.pdf';
 import hool_loyalty_nft_protocol from './Hool_loyalty_NFT_protocol.pdf';
+import hool_pitch_deck from './Hool_pitch_deck.pdf';
 
-export {hool_gameplay, hool_tokenomic, hool_loyalty_farming_protocol, hool_loyalty_nft_protocol};
+export {hool_gameplay, hool_tokenomic, hool_loyalty_farming_protocol, hool_loyalty_nft_protocol, hool_pitch_deck};
